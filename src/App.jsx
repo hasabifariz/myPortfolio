7e0c01599a98a8145cf5decca33e5e2f2f2fdf5f
hasabifariz <salt.tech4@aryanoble.co.id>
2024-11-01 15:58:10 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Project';
@@ -16,14 +16,16 @@ const App = () => {
   localStorage.setItem('theme', 'light')
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<Layout><About /></Layout>} /> */}
-        <Route path="/projects" element={<Layout><Projects /></Layout>} />
-        {/* <Route path="/contact" element={<Layout><Contact /></Layout>} /> */}
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<Layout><About /></Layout>} /> */}
+          <Route path="/projects" element={<Layout><Projects /></Layout>} />
+          {/* <Route path="/contact" element={<Layout><Contact /></Layout>} /> */}
+        </Routes>
+      </Router>
+    </BrowserRouter>
   );
 };
 
